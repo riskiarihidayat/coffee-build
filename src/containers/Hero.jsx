70f1d0098df8a72a5d9e2e88c8e3c08dfc9a1318
riskiarihidayat <riskiarihdyt@gmail.com>
2023-11-28 19:28:22 +0700

    add product page

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -7,7 +7,7 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 
 export default function Hero() {
   return (
-    <section className='bg-brown pt-28 px-6 md:flex md:justify-between md:items-center md:gap-[72px] md:px-44'>
+    <section className='bg-brown pt-28 px-6 relative sm:px-16 md:pb-20 md:flex md:justify-between md:items-center md:gap-[72px] md:px-44'>
       <div className='mb-8 md:order-2'>
         <div className='text-center mb-9 md:text-start md:mb-10'>
           <p className='font-sans text-mob-h4 text-sage font-medium mb-3 md:text-desk-h4'>
@@ -24,7 +24,7 @@ export default function Hero() {
             </h1>
           </div>
         </div>
-        <p className='font-sans text-mob-h4 font-normal text-white text-center mb-8 md:text-start md:text-desk-h4 md:mb-12'>
+        <p className='paragraph mb-8 md:text-start md:text-desk-h4 md:mb-12'>
           Each select coffee bean reflects our commitment to Peruvian coffee
           growers, who bring the best select coffee to your table.
         </p>
@@ -36,6 +36,7 @@ export default function Hero() {
       <div className='pb-28 m-auto md:order-1'>
         <img src={HeroCoffee} alt="Coffee Product Illustrator" className='md:w-[550px] ' />
       </div>
+      <div className='section-rounded bg-light-brown'></div>
     </section>
   )
 }
--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,5 +1,47 @@
 import React from 'react'
 
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+
+import product1 from './../assets/product-1.png'
+import product2 from './../assets/product-2.png'
+import product3 from './../assets/product-3.png'
+
 export default function Products() {
-  return <div>Products</div>
+  return (
+    <section className='relative bg-light-brown px-10 pb-24 md:flex md:items-center md:px-44 md:pb-32'>
+      <div className='mb-14 md:flex-1'>
+        <button className='flex items-center gap-2 bg-brown py-4 px-7 rounded-full mb-8 m-auto md:text-start md:m-0 md:mb-8'>
+          <p className='font-sans text-mob-h4 font-medium text-white md:text-desk-h4'>Scroll Down</p>
+          <MdOutlineKeyboardArrowRight className='text-2xl text-white rotate-90' />
+        </button>
+        <p className='paragraph md:text-start'>We strive to form deep partnerships with farmers from all over the world to create perspective together and form healthy working relationships built on trust and respect.</p>
+      </div>
+      <div className='flex md:flex-[2] md:justify-end'>
+        <div className='flex flex-wrap justify-center items-center gap-14'>
+          <div className='text-center'>
+            <img src={product1} alt="Coffee Product" className='w-[120px] mb-6' />
+            <div>
+              <p className='mob-product-detail'>Classic Coffee</p>
+              <p className='mob-product-detail'>$17.90</p>
+            </div>
+          </div>
+          <div className='text-center'>
+            <img src={product2} alt="Coffee Product" className='w-[120px] mb-6' />
+            <div>
+              <p className='mob-product-detail'>Black Coffee</p>
+              <p className='mob-product-detail'>$24.90</p>
+            </div>
+          </div>
+          <div className='text-center'>
+            <img src={product3} alt="Coffee Product" className='w-[120px] mb-6' />
+            <div>
+              <p className='mob-product-detail'>Strong Coffee</p>
+              <p className='mob-product-detail'>$32.90</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='section-rounded bg-white'></div>
+    </section>
+  )
 }
